775f4ba1a73f4efcfd8113cc943e590fbcc95b1a
patkasaw <patrycja.sawicka52@gmail.com>
2025-01-24 14:24:51 +0100

    added try

--- menu_functions.py
+++ menu_functions.py
@@ -38,8 +38,7 @@ def add_book():
 
 def show_books():
     if books == []:
-        print('The list is empty.')
-        return
+        raise ReferenceError('The list is empty')
     
     for index, book in enumerate(books):
         print(f'{index + 1}. {book.title}')
@@ -61,42 +60,50 @@ def get_book_index(message):
     return book_index  
 
 def update_book():
-    show_books()
+    try:
+        show_books()
+    except ReferenceError as e:
+        print(e)
+        return
+    
     book_index = get_book_index('Enter the number of the book you want to update:')
 
-    if 0 < book_index <= len(books):
-        book_to_update = books[book_index - 1]
-        print(f'1.Book title:{book_to_update.title}')
-        print(f'2.Book pages:{book_to_update.pages}')
-        print(f'3.Book reading time:{book_to_update.reading_time}')
-        update_choice = int(input('Which data do you want to update ? Enter your choice:'))
-        if update_choice == 1:
-            new_book_title = input(str('New book title:'))
-            book_to_update.title = new_book_title
-            print('Title has been changed')
-
-        if update_choice == 2:
-            try:
-                new_book_pages = int(input('New book pages:'))
-            except ValueError:
-                print('Given value is not a number. Please try again.')
-                return
-            book_to_update.pages = new_book_pages
-            print('Pages has been updated')
-        
-        if update_choice == 3:
-            try:
-                new_book_reading_time = int(input('New book reading time:'))
-            except ValueError:
-                print('Given value is not a number. Please try again.')
-                return
-            book_to_update.reading_time = new_book_reading_time
-            print('Reading time has been updated')
+    book_to_update = books[book_index - 1]
+    print(f'1.Book title:{book_to_update.title}')
+    print(f'2.Book pages:{book_to_update.pages}')
+    print(f'3.Book reading time:{book_to_update.reading_time}')
+    update_choice = int(input('Which data do you want to update ? Enter your choice:'))
+    if update_choice == 1:
+        new_book_title = input(str('New book title:'))
+        book_to_update.title = new_book_title
+        print('Title has been changed')
+    if update_choice == 2:
+        try:
+            new_book_pages = int(input('New book pages:'))
+        except ValueError:
+            print('Given value is not a number. Please try again.')
+            return
+        book_to_update.pages = new_book_pages
+        print('Pages has been updated')
+    
+    if update_choice == 3:
+        try:
+            new_book_reading_time = int(input('New book reading time:'))
+        except ValueError:
+            print('Given value is not a number. Please try again.')
+            return
+        book_to_update.reading_time = new_book_reading_time
+        print('Reading time has been updated')
 
     file.write_file(books)
 
 def delete_book():
-    show_books()
+    try:
+        show_books()
+    except ReferenceError as e:
+        print(e)
+        return
+    
     book_index = get_book_index('Enter the number of the book you want to delete:')
 
     books.pop(book_index - 1)
